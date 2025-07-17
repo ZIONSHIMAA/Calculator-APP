06ac374e8e29af2aad456c95d00f9fc0b69d5859
ZIONSHIMAA <ukyangchat@gmail.com>
2025-07-16 21:56:05 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const display = document.getElementById
+  ('display');
+const clearButton = document.getElementById
+  ('clear');
+const deleteButton = document.getElementById
+  ('delete');
+const equalsButton = document.getElementById
+  ('equals');
+const numberButton = document.querySelectorAll
+  ('#zero,#one,#two,#three,#four,#five,#six,#seven,#eight,#nine');
+const operatorButtons = document.querySelectorAll 
+  ('#plus,#minus,#multiply,#divide');
+
+
+   let currentNumber = ";
+   let previousNumber = ";
+   let currentOperator = null;
+
+   numberButtons.forEach(button=>{
+    button.addEventlistener('click',()=>{
+     currentNumber += button.textContent;
+     display.value = currentNumber;
+    });
+   });
+   
